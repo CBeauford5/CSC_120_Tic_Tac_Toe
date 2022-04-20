79727c38c3b2916d4a0421dda4967b23a67e887d
Tom Reaves <tomreaves@Toms-Macintosh.local>
2022-04-20 07:43:43 -0400

    Lab 9 Part 2

--- board.py
+++ board.py
@@ -2,12 +2,14 @@ board = [["-" for i in range(3)] for j in range(3)]
 
 
 def print_board():
+    print(' ')
     for x in board:
         print(x)
+    print(' ')
 
 
 def check_mark(row, col):
-    if board[row][col] == '-':
+    if board[int(row)][int(col)] == '-':
         return True
     else:
         return False
@@ -15,9 +17,9 @@ def check_mark(row, col):
 
 def place_mark(row, col, player_id):
     if player_id == 1:
-        board[row][col] = 'X'
+        board[int(row)][int(col)] = 'X'
     if player_id == 2:
-        board[row][col] = 'O'
+        board[int(row)][int(col)] = 'O'
 
 
 def check_win(player_id):
@@ -59,29 +61,72 @@ def check_win(player_id):
 
 
 def main():
-    print_board()
-    print(check_mark(0, 1))
-    print('')
+    turn_num = 0
+    while not check_win(1) or not check_win(2):
 
-    place_mark(0, 0, 2)
-    place_mark(0, 1, 2)
-    place_mark(0, 2, 2)
-    print_board()
-    print(check_win(2))
-    print('')
+        print_board()
+        row1 = input('Player 1: Please enter a row to place your X (0-2): ')
+        while row1 not in ['0', '1', '2']:
+            print('Please enter a valid row number.')
+            row1 = input('Player 1: Please enter a row to place your X (0-2): ')
 
-    place_mark(0, 2, 1)
-    place_mark(1, 2, 1)
-    place_mark(2, 2, 1)
-    print_board()
-    print(check_win(1))
-    print('')
+        col1 = input('Player 1: Please enter a column to place your X (0-2): ')
+        while col1 not in ['0', '1', '2']:
+            print('Please enter a valid column number.')
+            col1 = input('Player 1: Please enter a column to place your X (0-2): ')
 
-    place_mark(0, 2, 2)
-    place_mark(1, 1, 2)
-    place_mark(2, 0, 2)
-    print_board()
-    print(check_win(2))
+        while not check_mark(row1, col1):
+            print('This space is occupied. Choose another space.')
+            row1 = input('Player 1: Please enter a row to place your X (0-2): ')
+            while row1 not in ['0', '1', '2']:
+                print('Please enter a valid row number.')
+                row1 = input('Player 1: Please enter a row to place your X (0-2): ')
+
+            col1 = input('Player 1: Please enter a column to place your X (0-2): ')
+            while col1 not in ['0', '1', '2']:
+                print('Please enter a valid column number.')
+                col1 = input('Player 1: Please enter a column to place your X (0-2): ')
+
+        place_mark(row1, col1, 1)
+        if check_win(1):
+            print_board()
+            print('Player 1 has won the game!')
+            break
+        turn_num += 1
+        if turn_num == 9:
+            print_board()
+            print('This game was a tie!')
+            break
+
+        print_board()
+        row2 = input('Player 2: Please enter a row to place your O (0-2): ')
+        while row2 not in ['0', '1', '2']:
+            print('Please enter a valid row number.')
+            row2 = input('Player 2: Please enter a row to place your O (0-2): ')
+
+        col2 = input('Player 2: Please enter a column to place your O (0-2): ')
+        while col2 not in ['0', '1', '2']:
+            print('Please enter a valid column number.')
+            col2 = input('Player 2: Please enter a column to place your O (0-2): ')
+
+        while not check_mark(row2, col2):
+            print('This space is occupied. Choose another space.')
+            row2 = input('Player 2: Please enter a row to place your O (0-2): ')
+            while row2 not in ['0', '1', '2']:
+                print('Please enter a valid row number.')
+                row2 = input('Player 2: Please enter a row to place your O (0-2): ')
+
+            col2 = input('Player 2: Please enter a column to place your O (0-2): ')
+            while col2 not in ['0', '1', '2']:
+                print('Please enter a valid column number.')
+                col2 = input('Player 2: Please enter a column to place your O (0-2): ')
+
+        place_mark(row2, col2, 2)
+        if check_win(2):
+            print_board()
+            print('Player 2 has won the game!')
+            break
+        turn_num += 1
 
 
 main()
